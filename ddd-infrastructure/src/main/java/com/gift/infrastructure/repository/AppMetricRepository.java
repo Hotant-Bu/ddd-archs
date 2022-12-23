e70fe14e57716fa26005a3ac36eb79e013503fbc
gift <907011673@qq.com>
2022-12-23 20:09:01 +0800

    优化导入

--- ddd-infrastructure/src/main/java/com/gift/infrastructure/repository/AppMetricRepository.java
+++ ddd-infrastructure/src/main/java/com/gift/infrastructure/repository/AppMetricRepository.java
@@ -1,7 +1,6 @@
 package com.gift.infrastructure.repository;
 
 import com.gift.infrastructure.gatewayimpl.rpc.AppMetricMapper;
-import com.gift.infrastructure.gatewayimpl.rpc.dataobject.AppMetricDO;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
